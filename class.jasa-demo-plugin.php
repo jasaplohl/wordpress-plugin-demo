a8c2c66aa0dfa8696a5b519bc69b423872e7d4ef
jasa-plohl <jasa.plohl@devleet.si>
2022-11-25 13:16:44 +0100

    Loading the styles (css) and scripts (js).

--- class.jasa-demo-plugin.php
+++ class.jasa-demo-plugin.php
@@ -1,8 +1,12 @@
 <?php
 
 class JasaDemoPlugin {
+    public function __construct(){
+        add_action('init', array('JasaDemoPlugin', 'generateTransactionPostType'));
+        add_action('admin_enqueue_scripts', array('JasaDemoPlugin', 'enqueueAssets'));
+    }
+
     public static function pluginActivation(): void {
-//        $this->generateTransactionPostType();
         flush_rewrite_rules();
     }
 
@@ -19,5 +23,10 @@ class JasaDemoPlugin {
                 'menu_icon' => 'dashicons-money-alt'
             )
         );
+    }
+
+    public static function enqueueAssets() {
+        wp_enqueue_style('myPluginStyle', plugins_url('/assets/styles.css', __FILE__));
+        wp_enqueue_script('myPluginScript', plugins_url('/assets/helper.js', __FILE__));
     }
 }
