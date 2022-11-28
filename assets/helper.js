77c52ade7911c012bad4180c3e0504b5471d6263
jasa-plohl <jasa.plohl@devleet.si>
2022-11-28 11:30:42 +0100

    Switching between tabs on admin dashboard.

--- assets/helper.js
+++ assets/helper.js
@@ -1,1 +1,16 @@
-console.log("Javascript successfully loaded!");
+window.onload = () => {
+    const navbar = document.getElementById("navbar");
+    let activeTab =  0;
+    console.log(activeTab)
+    for(const tab of navbar.children) {
+        tab.addEventListener("click", switchTabs);
+    }
+}
+
+const switchTabs = (event) => {
+    document.querySelector("#navbar>.active").classList.remove("active");
+    document.querySelector(".tab-content>.active").classList.remove("active");
+    event.currentTarget.classList.add("active");
+    const tab = event.currentTarget.getAttribute("data-tab");
+    document.getElementById(tab).classList.add("active");
+};
--- templates/admin.php
+++ templates/admin.php
@@ -1,14 +1,28 @@
 <div class="wrap">
     <h1>Jasa Demo Plugin</h1>
     <?php settings_errors(); ?>
-    <form
-        method="post"
-        action="options.php"
-    >
-        <?php
-            settings_fields('userGroup');
-            do_settings_sections('jasa_demo');
-            submit_button();
-        ?>
-    </form>
+
+    <ul id="navbar" class="nav nav-tabs">
+        <li class="active" data-tab="tab-1">
+            <p>Settings</p>
+        </li>
+        <li  data-tab="tab-2">
+            <p>About</p>
+        </li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="tab-1" class="tab active">
+            <form method="post" action="options.php">
+		        <?php
+                    settings_fields('userGroup');
+                    do_settings_sections('jasa_demo');
+                    submit_button();
+		        ?>
+            </form>
+        </div>
+        <div id="tab-2" class="tab">
+            <h3>About tab.</h3>
+        </div>
+    </div>
 </div>
